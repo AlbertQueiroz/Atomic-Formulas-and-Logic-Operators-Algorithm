073035f54e73e959673b21ca4aa3946fe56909b6
Albert Rayneer <rayneerqueiroz@gmail.com>
2019-09-10 15:42:01 -0300

    Atomics and Conditionals
    First ideas for conditionals

---
+++ conectivos.py
@@ -0,0 +1,11 @@
+atomicos = ['a','b','c','d','e','f','g','h']
+
+A = []
+
+def conectivos(A, atomicos):
+    if A in atomicos:
+        return 0
+    if type(A[1]) == str:
+        return conectivos(A[0],atomicos) + 1 + conectivos(A[2],atomicos)
+    if A[0] == '˜':
+        return conectivos(A[1],atomicos)
