2747741621e9801f4584cfc5137a3db6f412e91e
Albert Rayneer <rayneerqueiroz@gmail.com>
2019-09-10 22:02:06 -0300

    Operators Counting Working

---
+++ operators.py
@@ -0,0 +1,14 @@
+atomics = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','x','y','z']
+
+#(t^ (~x))>a
+A = [['t','^',['~','x']],'>','a']
+
+def operators(A, atomics):
+    if A in atomics:
+        return 0
+    if (A[1] not in atomics) & (type(A[1]) != list):
+        return operators(A[0],atomics) + operators(A[2],atomics) + 1
+    if A[0] == '~':
+        return operators(A[1],atomics) + 1
+
+print(operators(A,atomics))
