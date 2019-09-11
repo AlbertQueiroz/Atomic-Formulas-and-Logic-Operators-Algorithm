7d852a3e99e43e2032b884b441d7d9bf15ffc838
Albert Rayneer <rayneerqueiroz@gmail.com>
2019-09-10 23:18:52 -0300

    Comments added

--- operators.py
+++ operators.py
@@ -1,14 +1,15 @@
 atomics = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','x','y','z']
 
-#(t^ (~x))>a
+#Se o trem chegou tarde e não tinha taxi na estação, então José chegou atrasado.
+#(t^(~x))>a
 A = [['t','^',['~','x']],'>','a']
 
 def operators(A, atomics):
-    if A in atomics:
+    if (A in atomics):
         return 0
     if (A[1] not in atomics) & (type(A[1]) != list):
         return operators(A[0],atomics) + operators(A[2],atomics) + 1
-    if A[0] == '~':
+    if (A[0] == '~'):
         return operators(A[1],atomics) + 1
 
 print("Number of logical operators:",operators(A,atomics))
